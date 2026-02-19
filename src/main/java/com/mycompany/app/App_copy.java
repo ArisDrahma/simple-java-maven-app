4dac985c837179a56f40c881d1533d526a245b1e
ArisDrahma <aris.drahma@gmail.com>
2026-02-19 12:49:15 +0700

    Add Deploy stage

---
+++ src/main/java/com/mycompany/app/App_copy.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App_copy {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App_copy() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
